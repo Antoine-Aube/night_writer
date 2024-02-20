1bcd6bccb0f9356c4549703e6787affc897d6b86
Antoine Aube <aaube3@gmail.com>
2024-02-20 11:25:45 -0700

    initial commit

---
+++ lib/night_writer.rb
@@ -0,0 +1,10 @@
+
+message_file = File.read(ARGV[0], "r")
+braille_file = File.open(ARGV[1], "w")
+character
+
+File.readlines(message_file).each do |line|
+  
+end
+
+puts "Created #{ARGV[1]} containing "
